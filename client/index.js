fbaea7b73f863dc172653f3666a67f626402f8b8
Chris Richards <ripter001@gmail.com>
2018-09-03 13:13:22 -0700

    DISABLE WebSocket

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import { bind, wire } from 'hyperHTML';
 import diceFate from './render/diceFate.js';
+import { roll1dF, toTotal } from './rollFateDice.js';
+
 let userName = '';
 let socket = null;
 
@@ -7,12 +9,19 @@ let socket = null;
 elmOutput.value = '';
 // Listen for roll clicks
 window.btnRoll.addEventListener('click', function(event) {
-  if (!socket || socket.readyState !== socket.OPEN) { return; }
-  // Ask for a dice roll
-  socket.send(JSON.stringify({
-    type: 'roll',
-    userName,
-  }));
+  // if (!socket || socket.readyState !== socket.OPEN) { return; }
+  // // Ask for a dice roll
+  // socket.send(JSON.stringify({
+  //   type: 'roll',
+  //   userName,
+  // }));
+  //
+  const dice = roll1dF();
+  updateRolls({
+      userName,
+      dice,
+      total: dice.reduce(toTotal),
+  });
 });
 
 
@@ -24,49 +33,50 @@ btnLogin.addEventListener('click', function(event) {
   }
 
   userName = name;
-  document.body.setAttribute('state', 'pending');
-  window.socket = socket = new WebSocket(`ws:/${location.host}/websocket`);
+  login();
+  // document.body.setAttribute('state', 'pending');
+  // window.socket = socket = new WebSocket(`ws:/${location.host}/websocket`);
+  //
+  // socket.addEventListener('error', function(event) {
+  //   console.error('Socket Error', event);
+  // });
+  //
+  // // wait for connection
+  // socket.addEventListener('open', function() {
+  //   // Send login info
+  //   socket.send(JSON.stringify({
+  //     type: 'login',
+  //     userName,
+  //   }));
+  // });
 
-  socket.addEventListener('error', function(event) {
-    console.error('Socket Error', event);
-  });
+  // // If the connection is lost, update the ui to show it.
+  // socket.addEventListener('close', function() {
+  //   document.body.setAttribute('state', 'closed');
+  // });
 
-  // wait for connection
-  socket.addEventListener('open', function() {
-    // Send login info
-    socket.send(JSON.stringify({
-      type: 'login',
-      userName,
-    }));
-  });
-
-  // If the connection is lost, update the ui to show it.
-  socket.addEventListener('close', function() {
-    document.body.setAttribute('state', 'closed');
-  });
-
-  // If the connection is lost, update the ui to show it.
-  socket.addEventListener('message', function(event) {
-    let payload;
-    try {
-      payload = JSON.parse(event.data);
-    }
-    catch(e) {
-      throw new Error('Could not parse message', event);
-      return;
-    }
-
-    switch (payload.type) {
-      case 'response-roll':
-        updateRolls(payload);
-        break;
-      case 'response-login':
-        login();
-        break;
-      default:
-        console.log('message', payload);
-    }
-  });
+  // // If the connection is lost, update the ui to show it.
+  // socket.addEventListener('message', function(event) {
+  //   let payload;
+  //   try {
+  //     payload = JSON.parse(event.data);
+  //   }
+  //   catch(e) {
+  //     throw new Error('Could not parse message', event);
+  //     return;
+  //   }
+  //
+  //   switch (payload.type) {
+  //     case 'response-roll':
+  //       updateRolls(payload);
+  //       break;
+  //     case 'response-login':
+  //       login();
+  //       break;
+  //     default:
+  //       console.log('message', payload);
+  //   }
+  // });
 });
 
 function updateRolls(diceRoll) {
@@ -84,7 +94,6 @@ function updateRolls(diceRoll) {
   </div>`;
 
   elmOutput.prepend(elmItem);
-  // elmOutput.value = `${userName} rolled a ${total}\n` + elmOutput.value;
 }
 
 
